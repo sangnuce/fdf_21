c8ffdde7691df3fc0b07bc477a3434003e344725
Nguyen Huyen <nguyen.my.huyen@framgia.com>
2016-10-31 09:42:47 +0700

    login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#show", page: "home"
   get "/static_pages/*page",to: "static_pages#show"
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create! name: "Admin", email: "huyennm195@gmail.com",
+  password: "123456", password_confirmation: "123456",
+  phone: "0123456789", admin: true,
+  activated: true, activated_at: Time.zone.now
+
+User.create! name: "Customer", email: "test@gmail.com",
+  password: "123456", password_confirmation: "123456",
+  phone: "0123456789", activated: true, activated_at: Time.zone.now
