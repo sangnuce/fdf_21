10f501d2cee8752d288f0310601e2041f6355c05
Nguyen Huyen <nguyen.my.huyen@framgia.com>
2016-11-01 08:21:35 +0700

    remember password, reset password

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,4 +3,9 @@ class UserMailer < ApplicationMailer
     @user = user
     mail to: user.email, subject: t("mailer.active_account")
   end
+
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t("mailer.password_reset")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   post "/signup", to: "users#create"
 
   resources :account_activations, only: :edit
+  resources :password_resets, except: [:index, :show, :destroy]
 end
